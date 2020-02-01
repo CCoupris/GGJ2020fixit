a9bd827d8ed8561678c1a9c6c5c56fa0305bf025
Teo Diaz <diaz.teo@outlook.com>
2020-02-02 00:14:17 +0100

    add table pour celui qui n'as pas de table

---
+++ Assets/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    protected enum menuButtonState
+    {
+        play,
+        option,
+        credit,
+        quit
+    }
+
+    [SerializeField]
+    protected Button play, option, quit, credit;
+
+    [SerializeField]
+    protected Button previous, next;
+
+
+    private menuButtonState buttonState;
+
+    protected menuButtonState ButtonState 
+    { 
+        get => buttonState; 
+        set => buttonState = value; 
+    }
+
+    protected void Awake()
+    {
+        play.onClick.AddListener(OnPlay);
+        play.onClick.AddListener(OnOption);
+        quit.onClick.AddListener(OnQuit);
+        credit.onClick.AddListener(OnCredit);
+        previous.onClick.AddListener(OnPrevious);
+        next.onClick.AddListener(OnNext);
+    }
+
+    private void OnNext()
+    {
+
+    }
+
+    private void OnPrevious()
+    {
+
+    }
+
+    private void OnCredit()
+    {
+        
+    }
+
+    private void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    private void OnOption()
+    {
+
+    }
+
+    private void OnPlay()
+    {
+
+    }
+}
