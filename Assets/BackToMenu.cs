92e19ef1b069d6779887aea266738ebbf175c082
Teo Diaz <diaz.teo@outlook.com>
2020-02-02 14:44:16 +0100

    FINAL

---
+++ Assets/BackToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+
+    public void GoBackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
